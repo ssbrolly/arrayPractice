a463fbcfad542472e9253daa78d50ae8f4cd9028
Usong Cho <silvermouse4012@gmail.com>
2018-09-24 19:11:33 -0700

    practice

--- practice.js
+++ practice.js
@@ -32,30 +32,78 @@ const cars = [
 
 
 // // 3. count the number of cars that were built on or before year 2000 (hint:  reduce)
-let count = 0;
-cars.forEach(car => {
-    if(car.year <= 2000) {
-        count++;
-    };
-});
-console.log(count);
+// let count = 0;
+// cars.forEach(car => {
+//     if(car.year <= 2000) {
+//         count++;
+//     };
+// });
+// console.log(count);
 
-let count1 = cars.reduce(total, car => {
-    if(car.year <= 2000){
-        total++;
-    };
-    return total;
-}, 0);
-console.log(count1);
+// let count1 = cars.reduce((total, car) => {
+//     if(car.year <= 2000){
+//         total++;
+//     };
+//     return total;
+// }, 0);
+// console.log(count1);
+
+// function oldCars(total,car) {
+//     if(car.year <= 2000) {
+//         total++;
+//     };
+//     return total;
+// };
+
+// console.log(cars.reduce(oldCars, 0));
 
 
 
 //  // 4. Grab just 'model' and 'year' of the cars that were built after year 2000 and save into a new array
 
+// let modelYear = [];
+// cars.forEach(car => {
+//     if(car.year > 2000) {
+//         let modelYearObject = {
+//             model: car.model,
+//             year: car.model,
+//         };
+//         modelYear.push(modelYearObject);
+//     };
+// });
+
+// console.log(modelYear);
+
+// let newModel = cars.map(car => {
+//     if(car.year > 2000) {
+//         let object = {
+//             model: this.model,
+//             year: this.year,
+//         };
+//         return object
+//     };
+// });
+// console.log(newModel);
+
+// let newerModel = cars.filter(car => {
+//     return car.year > 2000;
+// }).map(car => {
+//     return {
+//         model: car.model,
+//         year: car.year,
+//     };
+// });
+// console.log(newerModel);
+
 
 
 //  5. sort the cars array by model name in ascending order
-  //     hint:  make case insensitive.  4th item "camry" has lower case.  Make sure the sort results in correct ascending order ignoring the case.
+  // hint:  make case insensitive.  4th item "camry" has lower case.  Make sure the sort results in correct ascending order ignoring the case.
+cars.forEach(car => {
+    car.sort();
+    console.log(car.model);
+})
+
 
 
 
