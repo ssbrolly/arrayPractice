e2efecbdce1856a3a15b4874277db105a8b87928
Usong Cho <silvermouse4012@gmail.com>
2018-12-13 18:23:06 -0800

    practice

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ cars.forEach(car => {
 });
 
 // 2. filter all cars that were built after year 2000
-cars.forEach
+cars.forEach(car => {
+    
+})
 
 
 // 3. count the number of cars that were built on or before year 2000 (hint:  reduce)
